0f6e32dea65c1ec3ee9beda67a31f22daba590f3
vagrant <mikegithub77@gmail.com>
2019-05-28 22:28:26 +0000

    implemented a solution to recursive addition of nested array

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  const currentVal = array.pop()
+  if(Array.isArray(currentVal)) {
+    return sumItems(array) + sumItems(currentVal);
+  }
+  else if(currentVal === undefined) {
+    return 0;
+  }
+  return currentVal + sumItems(array);
 }
-
 module.exports = sumItems;
